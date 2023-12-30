e2195d38b4db58470b8c7f17c4593ec6c9f36f7a
Noah Kiser <noah.kiser@gmail.com>
2023-12-30 09:26:31 -0600

    asher update

--- boys/asher.php
+++ boys/asher.php
@@ -1,9 +1,14 @@
-<h2>Asher</h2>
+<h2>Asher is great </h2>
 <?php
-echo "asher is great" ;
+echo "asher is handsome" ;
 echo "<br>";
 echo "asher's favorite color is green";
 echo "<br>";
 $socks = "food";
 echo "asher likes to drink but asher really likes to eat $socks";
+echo "<br>";
+echo "asher is the smartest person on earth";
+echo "<br>";
+$burger = "clothes";
+echo "asher has amazing $burger";
 ?>
